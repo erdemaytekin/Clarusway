618c2e69c23c4d6506a00bd744a185c0af71764b
erdemaytekin <116784884+erdemaytekin@users.noreply.github.com>
2022-12-17 14:06:36 +0300

    17.12

---
+++ loops/1-for.js
@@ -0,0 +1,46 @@
+// * =======================================================
+// *             FOR LOOP
+// * =======================================================
+
+console.log(" **** FOR ****");
+
+// //? Ornek: 1 den n kadar sayilari toplayan kodu yaziniz//
+// const n=Number(prompt("Enter a number"))
+// let sum=0;for( let i=0;i<=n;i++){
+// sum+=i;
+// }
+// console.log("Sum",sum);
+
+//? Ornek: 0-100 arasinda n adet rastgele tamsayi ureten kpdu for dongusu ile yaziniz//
+
+// const num = Number(prompt("Enter a number"));
+// // let randomNum=0
+// for(let j=1; j<=num; j++){
+//   const  randomNum=Math.round(Math.random()*100);
+//     console.log(`${j}. random number:${randomNum}`);
+// }
+
+//? Örnek:Girilen bir sayinin Asal sayi olup olmadigini yazdıran kodu for donguleri kullanarak bulunuz.
+
+// const primeNum = Number(prompt("Enter a number"));
+// //Flag method
+// let isPrime=true;
+
+// if(primeNum<2){
+//     console.log("Number should be bigger than 1");
+// }else{
+
+
+// for(let i=2; i<primeNum;i++){
+//     if(primeNum%i===0){ // tam bolunuyorsa asal degildir
+//         console.log(`${primeNum} is not a prime number-${i}`);
+//         isPrime=false;
+//         break;
+//     }
+// }
+// if (isPrime === true) {
+//   console.log(`${primeNum} is a prime number`);
+// }
+// }
+
+
---
+++ loops/2-While.js
@@ -0,0 +1,11 @@
+// * ============================================
+// *            DONGULER(LOOPS)- WHILE
+// * ============================================
+
+console.log("****** WHILE *******");
+
+let count =1;
+while(count<=10){
+    console.log("Count:",count);
+    count++;
+}
