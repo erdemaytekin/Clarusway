ecd9b878e14872cb633f3020d6317a44c7ebc4c5
erdemaytekin <116784884+erdemaytekin@users.noreply.github.com>
2022-12-20 22:59:05 +0300

    20.12

--- loops/2-While.js
+++ loops/2-While.js
@@ -1,11 +1,43 @@
 // * ============================================
 // *            DONGULER(LOOPS)- WHILE
 // * ============================================
+// );
 
-console.log("****** WHILE *******");
+// console.log("****** WHILE *******"
+// let count =1;
+// while(count<=10){
+//     console.log("Count:",count);
+//     count++;
+// }
 
-let count =1;
-while(count<=10){
-    console.log("Count:",count);
-    count++;
-}
+// let grade=Number(prompt("Please enter your grade"));
+// while(grade<0 || 100<grade){
+//     console.log("İncorrect grade");
+//    grade = Number(prompt("Grade should be 0-100"));
+
+// }
+// console.log(`your grade ${grade}`);
+
+// let counter=0;
+// let number=Number(prompt("your number"));
+// while(number !=0){
+//     number=Math.trunc(number/10);
+//     // console.log(number);
+//     counter++;
+// }
+// console.log(`Digit count is ${counter}`);
+
+// * ============================================
+// *            DONGULER(LOOPS)- WHILE
+// * ============================================
+let again = "";
+do {
+  let avg = 0;
+  const midterm = Number(prompt("your midterm grade:"));
+  const final = Number(prompt("your final grade:"));
+  avg = midterm * 0.4 + final * 0.6;
+  console.log(`Your score is ${avg}`);
+   again = prompt("Do you want to continue(e/E)");
+} while (again === "e" || again === "E");
+console.log("See you later.Bye");
+
---
+++ loops/3-nestedLoops.js
@@ -0,0 +1,36 @@
+// * ============================================
+// *            NESTED LOOPS
+// * ============================================
+
+// console.log("** NESTED FOR **");
+// for(let i=0;i<3;i++){
+//     for(let j=0;j<2;j++){
+//         console.log(i,j);
+//     }
+// }
+
+
+//   let result;
+// 10+10 kere doner
+// for( let row=1;row<10;row++){
+// console.log(row);
+// }
+//     for(let coloumn=1;coloumn<10;coloumn++){
+//    console.log(coloumn);
+// }
+
+  let result;
+  // 10/10*10 kere doner
+let line=""
+  for( let row=1;row<=10;row++){
+    for(let col=1;col<=10;col++){
+   result=col*row;
+   line+=result + "\t"; //? /t bir tab bosluk ekler
+//    console.log(line);
+   
+}
+// console.log("-");
+line+="\n"
+console.log(line);
+}
+
