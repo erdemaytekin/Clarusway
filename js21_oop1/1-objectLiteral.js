99b667b7026bb03a7477ba16e8d9aaac1ada9fbc
erdemaytekin <116784884+erdemaytekin@users.noreply.github.com>
2023-02-03 01:46:44 +0300

    03.02

--- js21_oop1/1-objectLiteral.js
+++ js21_oop1/1-objectLiteral.js
@@ -4,3 +4,27 @@
 
 //* Object Literals
 console.log("**** Object Literals ****");
+
+const book1={
+    title:"The Karamazov Brothers",
+    author:"Dostoevski",
+    year:1886,
+    getSummary:function(){
+        return   `${this.title} was written by ${this.author} in ${this.year}`
+    }
+}
+
+console.log(book1);
+console.log(book1.getSummary())
+console.log(book1.hasOwnProperty("year"))
+
+const book2 = {
+  title: "The Lily of Valley",
+  author: "Balzac",
+  year: 1842,
+  getSummary: function () {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  },
+};
+
+console.log(book2.getSummary())
