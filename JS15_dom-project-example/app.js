24e242c55b93df6c0c19e3ddfe91e77cdecef7a6
erdemaytekin <116784884+erdemaytekin@users.noreply.github.com>
2023-03-16 19:48:49 +0300

    Update app.js

--- JS15_dom-project-example/app.js
+++ JS15_dom-project-example/app.js
@@ -1,5 +1,4 @@
 // Değişkenler Variables
-
 const yourChoice = document.getElementById("your-choice");
 const pcChoice = document.getElementById("pc-choice");
 const select = document.querySelector(".select");
